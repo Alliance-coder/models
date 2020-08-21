0f1edc8356b831673673acd49c11cbc5759207d8
Shirley-April <shirleyapril23@gmail.com>
2020-08-21 18:41:14 +0300

    first commit

---
+++ greenskiosk/customers/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+
+# Register your models here.
+
+
+from .models import Customer, Shipping_Addresss
+
+admin.site.register(Shipping_Addresss)
+admin.site.register(Customer)
---
+++ greenskiosk/kiosks/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+
+# Register your models here.
+
+from .models import Kiosk, KioskOwner
+
+admin.site.register(KioskOwner)
+admin.site.register(Kiosk)
